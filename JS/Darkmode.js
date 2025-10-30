3ce1d3e024de36161e630e43a3456161c0b10423
FelipeValeriano21 <felspreisv@gmail.com>
2025-10-29 21:22:01 -0300

    feat: dark mode

---
+++ JS/Darkmode.js
@@ -0,0 +1,16 @@
+  const toggleButton = document.getElementById('toggleDarkMode');
+  const body = document.body;
+
+  // Verifica se o modo escuro estava ativado anteriormente
+  if (localStorage.getItem('dark-mode') === 'true') {
+    body.classList.add('dark-mode');
+    toggleButton.textContent = '☀️ Modo Claro';
+  }
+
+  toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    const isDarkMode = body.classList.contains('dark-mode');
+
+    toggleButton.textContent = isDarkMode ? '☀️ Modo Claro' : '🌙 Modo Escuro';
+    localStorage.setItem('dark-mode', isDarkMode);
+  });
